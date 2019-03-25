611775328eda42b431170b2823630ac954c285bc
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2019-03-25 11:57:54 -0500

    Liberar memoria y swap

---
+++ freeramcache.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Note, we are using "echo 3", but it is not recommended in production instead use "echo 1"
+# https://www.tecmint.com/clear-ram-memory-cache-buffer-and-swap-space-on-linux/
+
+#echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'
+#OR
+#$ su -c "echo 3 >'/proc/sys/vm/drop_caches' && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'" root
+
+su -c "echo 3 >'/proc/sys/vm/drop_caches' && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'" root
