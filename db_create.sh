93e793735c69587a18bf53ec73560ca8ff28d98f
Carlos Julio <carlosjulioperez@gmail.com>
2019-06-12 10:30:18 -0500

    Scripts para migración de bd

---
+++ db_create.sh
@@ -0,0 +1,38 @@
+createdb -O sql-ledger -E SQL_ASCII -T template0 acb1         
+createdb -O sql-ledger -E SQL_ASCII -T template0 ap-daxti     
+createdb -O sql-ledger -E SQL_ASCII -T template0 app19        
+createdb -O sql-ledger -E SQL_ASCII -T template0 apprueba     
+createdb -O sql-ledger -E SQL_ASCII -T template0 apra19prueba 
+createdb -O sql-ledger -E SQL_ASCII -T template0 apracomsa    
+createdb -O sql-ledger -E SQL_ASCII -T template0 apracomsa19  
+createdb -O sql-ledger -E SQL_ASCII -T template0 atver        
+createdb -O sql-ledger -E SQL_ASCII -T template0 atver19      
+createdb -O sql-ledger -E SQL_ASCII -T template0 avisusa      
+createdb -O sql-ledger -E SQL_ASCII -T template0 bc-daxti     
+createdb -O sql-ledger -E SQL_ASCII -T template0 bc19test     
+createdb -O sql-ledger -E SQL_ASCII -T template0 bcprueba     
+createdb -O sql-ledger -E SQL_ASCII -T template0 biolitsa     
+createdb -O sql-ledger -E SQL_ASCII -T template0 biomicro19   
+createdb -O sql-ledger -E SQL_ASCII -T template0 blutec       
+createdb -O sql-ledger -E SQL_ASCII -T template0 blutec19     
+createdb -O sql-ledger -E SQL_ASCII -T template0 bu_prueba    
+createdb -O sql-ledger -E SQL_ASCII -T template0 busi12       
+createdb -O sql-ledger -E SQL_ASCII -T template0 busicentury  
+createdb -O sql-ledger -E SQL_ASCII -T template0 busicentury19
+createdb -O sql-ledger -E SQL_ASCII -T template0 catver       
+createdb -O sql-ledger -E SQL_ASCII -T template0 catver19     
+createdb -O sql-ledger -E SQL_ASCII -T template0 catver_prueba
+createdb -O sql-ledger -E SQL_ASCII -T template0 dbenblaco    
+createdb -O sql-ledger -E SQL_ASCII -T template0 dbmychart    
+createdb -O sql-ledger -E SQL_ASCII -T template0 dbname       
+createdb -O sql-ledger -E SQL_ASCII -T template0 gambalit     
+createdb -O sql-ledger -E SQL_ASCII -T template0 langolit     
+createdb -O sql-ledger -E SQL_ASCII -T template0 ponte19      
+createdb -O sql-ledger -E SQL_ASCII -T template0 probol19     
+createdb -O sql-ledger -E SQL_ASCII -T template0 propar19     
+createdb -O sql-ledger -E SQL_ASCII -T template0 qualperu19   
+createdb -O sql-ledger -E SQL_ASCII -T template0 tradetecsa   
+createdb -O sql-ledger -E SQL_ASCII -T template0 tradetecsa19 
+
+createdb -O sql-ledger -T template0 peru326      
+createdb -O sql-ledger -T template0 prueba327    
