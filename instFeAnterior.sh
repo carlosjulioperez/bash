dba9623e98d10bb2f2903d5e0bb11f123b96f750
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2019-12-09 11:35:47 -0500

    Script para instalar fe versión anterior

---
+++ instFeAnterior.sh
@@ -0,0 +1,27 @@
+SOURCE=/home/carper/feinst/feAnterior
+TOMCAT_HOME=/opt/tomcat-latest
+CIA=hpm
+
+DIR=$TOMCAT_HOME/webapps
+
+DIR1=$DIR/cjfews
+DIR2=$DIR/fe
+
+rm -rf $DIR1 $DIR2
+
+mkdir $DIR1
+mkdir $DIR2
+
+# Descomprimir en la carpeta destino
+# https://stackoverflow.com/questions/15830952/extracting-jar-to-specified-directory
+cd $DIR1
+jar -xvf $SOURCE/cjfews.war
+
+cd $DIR2
+jar -xvf $SOURCE/$CIA/fe.war
+
+chown -hR tomcat8: $DIR1 $DIR2
+
+cd $SOURCE
+ls -alrt $DIR
+tail -5 $DIR/fe/WEB-INF/classes/shiro.ini
